65f3edb9618bbdb7038f3cf701ebfbf06b9cf754
Dominik Vít <slipiklp@gmail.com>
2025-01-30 17:48:28 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,65 @@
+import { ArrowUpRight } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div className="min-w-screen md:min-h-screen">
+      <main className="flex flex-col md:grid grid-cols-12 max-w-7xl md:mx-auto w-full">
+        <div className="pt-32 md:pt-0 col-span-5 border-b md:border-b-0 md:border-l md:border-r md:min-h-screen h-full border-dashed border-zinc-800 px-6 flex justify-end flex-col">
+          <Image src="/logo.svg" alt="voidhash logo" width={128} height={20} />
+          <h1 className="mt-8 text-white text-6xl font-semibold max-w-sm">
+            We are voidhash#
+          </h1>
+          {/* Socials */}
+          <div className="flex gap-2 mt-8 pb-20 md:pb-32 -ml-2">
+            <Link
+              href="https://github.com/voidhashcom"
+              className="p-4 rounded-full bg-zinc-900/50 hover:bg-zinc-900"
+            >
+              <Image src="/github.svg" alt="GitHub" width={20} height={20} />
+            </Link>
+            <Link
+              href="https://x.com/voidhashcom"
+              className="p-4 rounded-full bg-zinc-900/50 hover:bg-zinc-900"
+            >
+              <Image src="/x-com.svg" alt="X.com" width={20} height={20} />
+            </Link>
+          </div>
+        </div>
+        <div className="col-span-6 justify-end pb-24 flex flex-col p-6 md:min-h-screen">
+          <h2 className="text-white text-xl font-semibold tracking-tight mb-4 ml-3">
+            Our projects
+          </h2>
+          <div className="flex flex-col gap-4 bg-zinc-900/50 p-8 rounded-xl">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/chiron.svg"
+              alt="Chiron logo"
+              width={147 * 0.8}
+              height={35 * 0.8}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <h3 className="sr-only">Chiron</h3>
+            <p className="text-2xl font-semibold mt-2 max-w-md">
+              Open-Source TypeScript subscription management library.
+            </p>
+            <div className="flex gap-2 mt-4">
+              <Link
+                href="https://chiron.sh"
+                className="p-4 flex space-x-2 border border-zinc-900 hover:bg-zinc-900 rounded-full"
+              >
+                <span className="uppercase font-semibold">Website</span>
+                <ArrowUpRight size={24} />
+              </Link>
+              <Link
+                href="https://github.com/voidhashcom/chiron"
+                className="p-4 rounded-full border border-zinc-900 bg-zinc-900/50 hover:bg-zinc-900"
+              >
+                <Image src="/github.svg" alt="GitHub" width={20} height={20} />
+              </Link>
+            </div>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
