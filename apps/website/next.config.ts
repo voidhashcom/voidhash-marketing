8c4b1a1ea06090d9b6acf23b0f3b208a91c8539a
Dominik Vít <slipiklp@gmail.com>
2025-05-07 12:01:04 +0200

    feat: refactor

---
+++ apps/website/next.config.ts
@@ -0,0 +1,19 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
+import path from 'node:path'
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {}
+
+if (process.env.NODE_ENV === 'development') {
+  nextConfig.outputFileTracingRoot = path.join(__dirname, '../../')
+}
+
+export default nextConfig
