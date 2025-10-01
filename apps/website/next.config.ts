303e66e600f894b69cca112c444267faff01dc9c
Dominik Vít <slipiklp@gmail.com>
2025-10-01 13:22:45 +0200

    feat: add docs path rewrite

--- apps/website/next.config.ts
+++ apps/website/next.config.ts
@@ -6,14 +6,29 @@
 
 // export default nextConfig;
 
+import path from 'node:path';
+import type { NextConfig } from 'next';
 
-import path from 'node:path'
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  // biome-ignore lint/suspicious/useAwait: async is required
+  rewrites: async () => {
+    return [
+      {
+        source: '/docs/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'voidhash.com',
+          },
+        ],
+        destination: 'docs.voidhash.com/docs/:path*',
+      },
+    ];
+  },
+};
 
 if (process.env.NODE_ENV === 'development') {
-  nextConfig.outputFileTracingRoot = path.join(__dirname, '../../')
+  nextConfig.outputFileTracingRoot = path.join(__dirname, '../../');
 }
 
-export default nextConfig
+export default nextConfig;
